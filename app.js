0a2dca2c1f0ab74678aacbbb89ce0c5ee48c7b3a
FrancisCKY <a231685600@gmail.com>
2023-07-23 12:59:11 +0800

    router init

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ app.get('/', (req, res) => {
   res.send("hello world!")
 })
 
+app.get('/todos', (req, res) => {
+  res.send('get all todos')
+})
+
+app.post('/todos', (req, res) => {
+  res.send('add todos')
+})
+
+app.put('/todos/:id', (req, res) => {
+  res.send(`todo id ${req.params.id} has been modified`)
+})
+
+app.delete('/todos/:id', (req, res) => {
+  res.send(`todo id ${req.params.id} has been deleted`)
+})
 
 app.listen(3000, () => {
   console.log('The app is running')
