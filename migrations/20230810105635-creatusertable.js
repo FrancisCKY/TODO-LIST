d4c49662c30350fc87d2fdee1e494c67d593cea2
FrancisCKY <a231685600@gmail.com>
2023-08-10 19:00:01 +0800

    add user_migration

---
+++ migrations/20230810105635-creatusertable.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {                                       /*系統預設所產生*/
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {                                /*系統預設所產生*/
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {                                /*系統預設所產生*/
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Todos');
+  }
+};
